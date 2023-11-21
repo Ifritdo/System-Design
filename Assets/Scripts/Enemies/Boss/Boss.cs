55a177809f54a6dfbe6621d166cb78f564b282b5
Ifritdo <JulianFirpo316@hotmail.com>
2023-11-21 18:51:30 -0300

    Fix1

--- Assets/Scripts/Enemies/Boss/Boss.cs
+++ Assets/Scripts/Enemies/Boss/Boss.cs
@@ -6,15 +6,18 @@ public class BossReference
 {
     public BossCanvasManager canvasManager;
     public BossHealthBar healthBar;
+    public ScoreManager scoreManager;  // Agrega una referencia al ScoreManager
 }
 
 public class Boss : MonoBehaviour
 {
     [SerializeField]
     private int maxHealth = 100;
-    
-    [SerializeField]
+
     private int currentHealth;
+
+    [SerializeField]
+    private int pointsValue = 50;  // Valor de puntos que otorga al ser derrotado
 
     public BossReference bossReference;
 
@@ -24,19 +27,18 @@ public class Boss : MonoBehaviour
     {
         currentHealth = maxHealth;
 
-        if (bossReference != null && bossReference.healthBar != null && bossReference.canvasManager != null)
+        if (bossReference != null && bossReference.healthBar != null && bossReference.canvasManager != null && bossReference.scoreManager != null)
         {
-            //healthBar = bossReference.healthBar;
-            //healthBar.SetMaxHealth(maxHealth);
-            //healthBar.SetHealth(currentHealth);
+            healthBar = bossReference.healthBar;
+            healthBar.SetMaxHealth(maxHealth);
+            healthBar.SetHealth(currentHealth);
 
             // Desactivar el objeto del Canvas al inicio
             bossReference.canvasManager.DeactivateBossCanvas();
         }
         else
         {
-            Debug.LogWarning("La referencia a la barra de salud del jefe o al Canvas Manager no está configurada correctamente.");
-            
+            Debug.LogWarning("La referencia a la barra de salud del jefe, al Canvas Manager o al Score Manager no está configurada correctamente.");
         }
     }
 
@@ -50,33 +52,56 @@ public class Boss : MonoBehaviour
 
     private void Die()
     {
-        Destroy(gameObject);
+        if (healthBar != null && bossReference.canvasManager != null && bossReference.scoreManager != null)
+        {
+            // Activar el objeto del Canvas cuando el jefe muere
+            bossReference.canvasManager.ActivateBossCanvas();
+
+            // Desactivar el slider cuando el jefe muere
+            healthBar.SetSliderActive(false);
+
+            // Otorgar puntos al ScoreManager
+            bossReference.scoreManager.AddPoints(GetPointsValue());
+
+            // Puedes agregar cualquier lógica adicional cuando el jefe muere
+            // Por ejemplo, reproducir una animación, mostrar un mensaje, etc.
+
+            gameObject.SetActive(false);
+        }
     }
-
-    //void Die()
-    //{
-    //    if (healthBar != null && bossReference.canvasManager != null)
-    //    {
-    //        // Activar el objeto del Canvas cuando el jefe muere
-    //        bossReference.canvasManager.ActivateBossCanvas();
-
-    //        // Desactivar el slider cuando el jefe muere
-    //        healthBar.SetSliderActive(false);
-    //    }
-
-    //    // Puedes agregar cualquier lógica adicional cuando el jefe muere
-    //    // Por ejemplo, reproducir una animación, mostrar un mensaje, etc.
-
-    //    gameObject.SetActive(false);
-    //}
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Bullet bullet = collision.GetComponent<Bullet>();
-
-        if (bullet != null && currentHealth > 0)
+        // Verifica la colisión con la bala del jugador
+        if (collision.CompareTag("PlayerBullet"))
         {
-            currentHealth -= bullet.damage;
+            Bullet bullet = collision.GetComponent<Bullet>();
+
+            if (bullet != null && currentHealth > 0)
+            {
+                // Restar la cantidad de daño a la salud actual
+                currentHealth -= bullet.damage;
+
+                // Actualizar la barra de salud si está asignada
+                if (healthBar != null)
+                {
+                    healthBar.SetHealth(currentHealth);
+                }
+
+                // Verificar si la salud llegó a cero
+                if (currentHealth <= 0)
+                {
+                    Die();
+                }
+            }
         }
+    }
+
+    // Resto del código...
+
+    // Método para obtener el valor en puntos del jefe
+    public int GetPointsValue()
+    {
+        return pointsValue;
     }
 }
